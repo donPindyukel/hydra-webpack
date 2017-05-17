1d88a68c463a35cf63de17fe47817952df08ac25
Aleksey Pleshkov <im@alekseypleshkov.ru>
2017-05-17 10:09:35 +0300

    Компонент слайдера переписан на плагин для jquery

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -12,3 +12,24 @@ $('.js-input').fieldEventValidate();
 $('.js-ajax-form').formAjax(function (data) {
     log(data);
 }, true);
+
+$('.js-lazy').lazyShowIfVisible();
+$('.js-lazy-img').lazyLoadImage('img', 'src', true, function () {
+    log('test');
+});
+
+$('.js-slider').sliderInit();
+
+$('.js-prev').click(function () {
+    $(this).sliderPrev();
+});
+
+$('.js-next').click(function () {
+    $(this).sliderNext();
+});
+
+$('.js-slide').click(function () {
+    let dataSlide = $(this).data('slide');
+    $(this).sliderOpenNumber(dataSlide);
+
+});
