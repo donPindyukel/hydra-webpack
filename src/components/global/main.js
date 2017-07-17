90c8dc55c0e2e509f21b7ef4e3c62fe9d45c54d3
Aleksey Pleshkov <im@alekseypleshkov.ru>
2017-07-17 09:06:16 +0300

    Очитска от тестового контента

--- src/components/global/main.js
+++ src/components/global/main.js
@@ -2,11 +2,3 @@
 // Глобальные скрипты
 // ==========================================================================
 
-let slider = $('.js-slider');
-slider.sliderInit('.slider__body', '.slider__item');
-
-slider.sliderAutoscroll();
-
-$('.js-slider-next').click(function () {
-    slider.sliderNext()
-});
