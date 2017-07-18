9bb8c8ec5d33fe353734d36773d2d6c9fa80a449
Aleksey Pleshkov <im@alekseypleshkov.ru>
2017-07-18 16:25:09 +0300

    #31 Добавить в hydra.js автоматическое обновление компонентов из репозитория.

--- hydra.js
+++ hydra.js
@@ -1,23 +1,26 @@
-var fs = require('fs');
+let fs = require('fs');
+let git = require("nodegit");
+let ncp = require("ncp").ncp;
+let rimraf = require('rimraf');
 
 // Тип команды
-var commandType = null;
-// Имя создаваемого блока из консоли
-var blockName = null;
+let commandType = null;
 
 // Получаем имя команды
-process.argv.forEach(function (val, index, array) {
+process.argv.forEach((val, index, array) => {
 	if (index == 2) {
 		commandType = val;
 	}
 });
 
-if (commandType == "block") {
+if (commandType == 'block') {
+	// Имя создаваемого блока из консоли
+	let blockName = null;
 	// Ссылки для создания элементов блока
-	var dir = "./src/blocks";
+	let dir = './src/blocks';
 
 	// Получаем имя блока для создания
-	process.argv.forEach(function (val, index, array) {
+	process.argv.forEach((val, index, array) => {
 		if (index == 3) {
 			blockName = val;
 		}
@@ -25,47 +28,88 @@ if (commandType == "block") {
 
 	// Если имя блока было указано, создаем элементы блока
 	if (blockName != null) {
-		fs.mkdir(dir + '/' + blockName, function (e) {
+		fs.mkdir(`${dir}/${blockName}`, (e) => {
 			if (e === null) {
 				// Создание директории с изображениями
-				fs.mkdir(dir + '/' + blockName + '/' + 'img', function (e) {});
+				fs.mkdir(`${dir}/${blockName}/` + 'img', (e) => {});
 
 				createFile(blockName, 'pug', dir);
 				createFile(blockName, 'scss', dir);
 				createFile(blockName, 'js', dir);
 
-				appendTextFile(dir + '/app.scss', '@import "' + blockName + "/" + blockName + '";');
-				appendTextFile(dir + '/app.js', "//@include('" + blockName + "/" + blockName + ".js')");
-			}
-			else {
-				console.log('Block "' + blockName + '" exists!');
+				appendTextFile(`${dir}/app.scss`, `@import "${blockName}/${blockName}";`);
+				appendTextFile(`${dir}/app.js`, `//@include('${blockName}/${blockName}.js')`);
+			} else {
+				console.log(`Block "${blockName}" exists!`);
 			}
 		});
-
 	}
 }
 
+if (commandType == 'update') {
+	// Ветка из которой обновить
+	let updateType = 'master';
+
+	// Получаем имя блока для создания
+	process.argv.forEach((val, index, array) => {
+		if (index == 3) {
+			updateType = val;
+		}
+	});
+
+	// Клонирование репозитория
+	let cloneOptions = new git.CloneOptions();
+	cloneOptions.checkoutBranch = updateType;
+	git.Clone('https://github.com/AlekseyPleshkov/hydra-webpack.git', './update', cloneOptions).then(function(repository) {
+		console.log('Репозиторий удачно склонирован.');
+
+		// Копируем файлы для обновления пакета
+		ncp('./update/src/components', './/src/components', function (err) {});
+		ncp('./update/src/components/components.js', './src/components/components.js', function (err) {});
+		ncp('./update/src/components/components.scss', './src/components/components.scss', function (err) {});
+		
+		ncp('./update/.csscomb.json', './.csscomb.json', function (err) {});
+		ncp('./update/.eslintrc.json', './.eslintrc.json', function (err) {});
+		ncp('./update/.flowconfig', './.flowconfig', function (err) {});
+		ncp('./update/.pug-lintrc.json', './.pug-lintrc.json', function (err) {});
+		ncp('./update/bower.json', './bower.json', function (err) {});
+		ncp('./update/gulpfile.js', './gulpfile.js', function (err) {});
+		
+		ncp('./updatehydra.js', './hydra.js', function (err) {});
+		ncp('./package.json', './package.json', function (err) {});
+
+		rimraf('./update', function() {
+			console.log('Временная директория удалена.');
+		});
+		
+		console.log('Копирование файлов завершено.');
+		console.log('Запустите "npm i && bower i".');
+	});
+};
+
 // Функция для создания файла
 function createFile(name, ext, directory) {
-	var content = "/\/ ==========================================================================" +
-		"\n/\/ " + name +
-		"\n/\/ ==========================================================================";
-	var dir = directory + "/" + name;
-	var file = dir + "/" + name + "." + ext;
+	let content = `${'/\/ ==========================================================================' +
+		'\n/\/ '}${name
+	}\n/\/ ==========================================================================`;
+	let dir = `${directory}/${name}`;
+	let file = `${dir}/${name}.${ext}`;
 
-	fs.writeFile(file, content, function (err) {
+	fs.writeFile(file, content, (err) => {
 		if (err) {
 			console.log(err);
 		} else {
-			console.log("The " + file + " was saved!");
+			console.log(`Файл ${file} успешно создан.`);
 		}
 	});
 }
 
 // Добавить текст в конец строки файла
 function appendTextFile(file, text) {
-	fs.appendFile(file, '\n' + text, function (err) {
-		if (err) throw err;
-		console.log('Modify ' + file + ' success');
+	fs.appendFile(file, `\n${text}`, (err) => {
+		if (err) {
+			throw err;
+		}
+		console.log(`Файл ${file} дополнен ссылкой на блок.`);
 	});
 }
