df48f08a4b57ca3a1c635347c6b2a9998f9f6ae3
Aleksey Pleshkov <im@alekseypleshkov.ru>
2017-08-02 10:23:28 +0300

    Удалил bower и его зависимости.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
-'use strict';
-
 //
 // Настройки
 //
 
 // Корневая директория
-var buildDir = "./dist";
+let buildDir = './dist';
 
 // Директория для сборки html/php
-var buildDirHtml = buildDir;
+let buildDirHtml = buildDir;
 
 // Директория для сборки css/js/images/data/fonts
-var buildDirStatic = buildDir + "/assets";
+let buildDirStatic = buildDir + '/assets';
 
 //
 // Сборщик
@@ -20,7 +18,6 @@ var buildDirStatic = buildDir + "/assets";
 var gulp = require('gulp'),
 	rename = require('gulp-rename'),
 	babel = require('gulp-babel'),
-	flow = require('gulp-flowtype'),
 	watch = require('gulp-watch'),
 	prefixer = require('gulp-autoprefixer'),
 	uglify = require('gulp-uglify'),
@@ -32,188 +29,194 @@ var gulp = require('gulp'),
 	pug = require('gulp-pug'),
 	eslint = require('gulp-eslint'),
 	puglint = require('gulp-pug-linter'),
-	browserify = require('browserify'),
-	vueify = require('vueify'),
 	pngquant = require('imagemin-pngquant'),
 	rimraf = require('rimraf'),
 	browserSync = require('browser-sync'),
 	reload = browserSync.reload,
 	plumber = require('gulp-plumber');
 
-var path = {
+let path = {
 	build: {
 		pug: buildDirHtml,
 		html: buildDirHtml,
 		php: buildDirHtml,
-		leaf: buildDirHtml,
-		js: buildDirStatic + '/js/',
-		css: buildDirStatic + '/css',
+		js: buildDirStatic,
+		css: buildDirStatic,
 		img: buildDirStatic + '/img/',
 		fonts: buildDirStatic + '/fonts/',
-		data: buildDirStatic + '/data/'
+		data: buildDirStatic + '/data/',
 	},
 	src: {
 		pug: ['src/pages/*.pug'],
 		html: ['src/pages/*.html'],
 		php: ['src/pages/*.php'],
-		leaf: ['src/pages/*.leaf'],
 		js: ['src/blocks/*.js'],
 		scss: ['src/blocks/*.scss'],
 		img: ['src/**/*.jpg', 'src/**/*.png', 'src/**/*.svg', 'src/**/*.gif', 'src/**/*.ico'],
 		fonts: ['src/fonts/*.*', 'src/fonts/**/*.*'],
-		data: ['src/data/*.*', 'src/data/**/*.*']
+		data: ['src/data/*.*', 'src/data/**/*.*'],
 	},
 	watch: {
 		pug: ['src/pages/*.pug', 'src/blocks/**/*.pug'],
 		html: ['src/pages/*.html', 'src/blocks/**/*.html'],
 		php: ['src/pages/*.php', 'src/blocks/**/*.php'],
-		leaf: ['src/pages/*.leaf', 'src/blocks/**/*.leaf'],
 		js: ['src/components/*.js', 'src/components/**/*.js', 'src/blocks/*.js', 'src/blocks/**/*.js'],
 		scss: ['src/components/*.scss', 'src/components/**/*.scss', 'src/blocks/*.scss', 'src/blocks/**/*.scss'],
 		img: ['src/**/*.jpg', 'src/**/*.png', 'src/**/*.svg', 'src/**/*.gif', 'src/**/*.ico'],
 		fonts: ['src/fonts/*.*', 'src/fonts/**/*.*'],
-		data: ['src/data/*.*', 'src/data/**/*.*'], 
+		data: ['src/data/*.*', 'src/data/**/*.*'],
 		jslint: ['src/blocks/*.js', 'src/blocks/**/*.js'],
 		puglint: ['src/pages/*.pug', 'src/blocks/**/*.pug'],
 	},
-	clean: buildDir
+	clean: buildDir,
 };
 
-gulp.task('connect-sync', function () {
+gulp.task('connect-sync', () => {
 	connectPhp.server({
-		base: buildDir
-	}, function () {
+		base: buildDir,
+	}, () => {
 		browserSync({
 			proxy: '127.0.0.1:8000',
 		});
 	});
 });
 
-gulp.task('clean', function (cb) {
+gulp.task('clean', (cb) => {
 	rimraf(path.clean, cb);
 });
 
-gulp.task('pug:build', function () {
+gulp.task('pug:build', () => {
 	gulp.src(path.src.pug)
 		.pipe(plumber())
 		.pipe(fileinclude({
 			prefix: '//@',
-			basepath: '@file'
+			basepath: '@file',
 		}))
 		.pipe(pug({
-			pretty: true
+			pretty: true,
 		}))
 		.pipe(gulp.dest(path.build.pug))
-		.pipe(reload({stream: true}));
+		.pipe(reload({
+			stream: true,
+		}));
 });
 
-gulp.task('html:build', function () {
+gulp.task('html:build', () => {
 	gulp.src(path.src.html)
 		.pipe(plumber())
 		.pipe(fileinclude({
 			prefix: '//@',
-			basepath: '@file'
+			basepath: '@file',
 		}))
 		.pipe(gulp.dest(path.build.html))
-		.pipe(reload({stream: true}));
+		.pipe(reload({
+			stream: true,
+		}));
 });
 
-gulp.task('php:build', function () {
+gulp.task('php:build', () => {
 	gulp.src(path.src.php)
 		.pipe(plumber())
 		.pipe(fileinclude({
 			prefix: '//@',
-			basepath: '@file'
+			basepath: '@file',
 		}))
 		.pipe(gulp.dest(path.build.php))
-		.pipe(reload({stream: true}));
+		.pipe(reload({
+			stream: true,
+		}));
 });
 
-gulp.task('leaf:build', function () {
-	gulp.src(path.src.leaf)
-		.pipe(plumber())
-		.pipe(fileinclude({
-			prefix: '//@',
-			basepath: '@file'
-		}))
-		.pipe(gulp.dest(path.build.leaf))
-		.pipe(reload({stream: true}));
-});
-
-gulp.task('js:build', function () {
+gulp.task('js:build', () => {
 	gulp.src(path.src.js)
 		.pipe(plumber())
 		.pipe(fileinclude({
 			prefix: '//@',
-			basepath: '@file'
+			basepath: '@file',
 		}))
 		.pipe(babel({
 			presets: ['es2015', 'es2016', 'es2017', 'flow'],
 		}))
 		.pipe(uglify())
 		.pipe(gulp.dest(path.build.js))
-		.pipe(reload({stream: true}));
+		.pipe(reload({
+			stream: true,
+		}));
 });
 
-gulp.task('scss:build', function () {
+gulp.task('scss:build', () => {
 	gulp.src(path.src.scss)
 		.pipe(plumber())
 		.pipe(sass({
 			includePaths: ['src/scss'],
 			outputStyle: 'compressed',
 			sourceMap: true,
-			errLogToConsole: true
+			errLogToConsole: true,
 		}))
 		.pipe(prefixer())
 		.pipe(cssmin())
 		.pipe(gulp.dest(path.build.css))
-		.pipe(reload({stream: true}));
+		.pipe(reload({
+			stream: true,
+		}));
 });
 
-gulp.task('image:build', function () {
+gulp.task('image:build', () => {
 	gulp.src(path.src.img)
 		.pipe(plumber())
-		.pipe(rename({dirname: ''}))
+		.pipe(rename({
+			dirname: '',
+		}))
 		.pipe(imagemin([
-			imagemin.gifsicle({interlaced: true}),
-			imagemin.jpegtran({progressive: true}),
-			imagemin.optipng({optimizationLevel: 5}),
-			imagemin.svgo({plugins: [{removeViewBox: true}]})
+			imagemin.gifsicle({
+				interlaced: true,
+			}),
+			imagemin.jpegtran({
+				progressive: true,
+			}),
+			imagemin.optipng({
+				optimizationLevel: 5,
+			}),
+			imagemin.svgo({
+				plugins: [{
+					removeViewBox: true,
+				}],
+			}),
 		]))
 		.pipe(gulp.dest(path.build.img))
-		.pipe(reload({stream: true}));
+		.pipe(reload({
+			stream: true,
+		}));
 });
 
-gulp.task('fonts:build', function () {
+gulp.task('fonts:build', () => {
 	gulp.src(path.src.fonts)
 		.pipe(plumber())
-		.pipe(gulp.dest(path.build.fonts))
+		.pipe(gulp.dest(path.build.fonts));
 });
 
-gulp.task('data:build', function () {
+gulp.task('data:build', () => {
 	gulp.src(path.src.data)
 		.pipe(plumber())
-		.pipe(gulp.dest(path.build.data))
+		.pipe(gulp.dest(path.build.data));
 });
 
 gulp.task('pug:lint', () => {
-	return gulp.src(path.watch.puglint)
+	gulp.src(path.watch.puglint)
 		.pipe(puglint())
-		.pipe(puglint.reporter())
+		.pipe(puglint.reporter());
 });
 
 gulp.task('js:lint', () => {
-	return gulp.src(path.watch.jslint)
+	gulp.src(path.watch.jslint)
 		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(flow({ abort: true }))
+		.pipe(eslint.format());
 });
 
 gulp.task('build', [
 	'pug:build',
 	'html:build',
 	'php:build',
-	'leaf:build',
 	'js:build',
 	'scss:build',
 	'fonts:build',
@@ -221,38 +224,35 @@ gulp.task('build', [
 	'data:build',
 ]);
 
-gulp.task('watch', function () {
-	watch(path.watch.pug, function (event, cb) {
+gulp.task('watch', () => {
+	watch(path.watch.pug, (event, cb) => {
 		gulp.start('pug:build');
 	});
-	watch(path.watch.html, function (event, cb) {
+	watch(path.watch.html, (event, cb) => {
 		gulp.start('html:build');
 	});
-	watch(path.watch.php, function (event, cb) {
+	watch(path.watch.php, (event, cb) => {
 		gulp.start('php:build');
 	});
-	watch(path.watch.leaf, function (event, cb) {
-		gulp.start('leaf:build');
-	});
-	watch(path.watch.js, function (event, cb) {
+	watch(path.watch.js, (event, cb) => {
 		gulp.start('js:build');
 	});
-	watch(path.watch.scss, function (event, cb) {
+	watch(path.watch.scss, (event, cb) => {
 		gulp.start('scss:build');
 	});
-	watch(path.watch.img, function (event, cb) {
+	watch(path.watch.img, (event, cb) => {
 		gulp.start('image:build');
 	});
-	watch(path.watch.fonts, function (event, cb) {
+	watch(path.watch.fonts, (event, cb) => {
 		gulp.start('fonts:build');
 	});
-	watch(path.watch.data, function (event, cb) {
+	watch(path.watch.data, (event, cb) => {
 		gulp.start('data:build');
 	});
-	watch(path.watch.js, function (event, cb) {
+	watch(path.watch.js, (event, cb) => {
 		gulp.start('js:lint');
 	});
-	watch(path.watch.pug, function (event, cb) {
+	watch(path.watch.pug, (event, cb) => {
 		gulp.start('pug:lint');
 	});
 });
